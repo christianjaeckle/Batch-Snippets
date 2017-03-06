d7816119cccc0c0398d8440a17b917c3bcad8112
Christian Jäckle <info@christianjaeckle.de>
2017-03-06 10:54:23 +0100

    Update Delete thumbs.db file recursively.cmd

--- File System/Delete thumbs.db file recursively.cmd
+++ File System/Delete thumbs.db file recursively.cmd
@@ -1,1 +1,7 @@
+ECHO./s = Delete specified files from all subdirectories
+ECHO./q = Don't ask for deleteion if global wildcards are being used
+ECHO./f = Force deleting also of read-only files
+ECHO./a = Delete hidden files
+
+ECHO.Delete thumbs.db recursively
 Del /s /q /f /a:h Thumbs.db
