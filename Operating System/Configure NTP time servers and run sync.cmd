2f1c7bff417f4241ff5011689dffa50104862c97
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2022-06-23 19:10:25 +0200

    Update Configure NTP time servers and run sync.cmd

--- Operating System/Configure NTP time servers and run sync.cmd
+++ Operating System/Configure NTP time servers and run sync.cmd
@@ -1,15 +1,15 @@
 ECHO.Stop the service
 Net.exe Stop w32time
 
-ECHO.Unregister and register again
+ECHO Unregister and register again
 w32tm /unregister
 w32tm /register
 
-ECHO.Start the service
+ECHO Start the service
 Net.exe Start w32time
 
-ECHO.Configure the service
-w32tm /config /update /manualpeerlist:"de.pool.ntp.org ptbtime1.ptb.de time.windows.com" /syncfromflags:manual
+ECHO Configure the service
+w32tm /config /manualpeerlist:"0.de.pool.ntp.org ptbtime1.ptb.de time.windows.com" /syncfromflags:manual /update
 
-ECHO.Run the synchronization
+ECHO Run the synchronization
 w32tm /resync
