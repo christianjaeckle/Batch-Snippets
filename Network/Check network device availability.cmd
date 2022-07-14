518b61fd499e0982e1dd8d4787efc293cb01c3d5
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2022-07-14 10:05:42 +0200

    Update Check network device availability.cmd

--- Network/Check network device availability.cmd
+++ Network/Check network device availability.cmd
@@ -3,11 +3,11 @@ SET IPAdresse=192.168.1.1
 
 PING -n 1 %IPAdresse% | find "TTL=" >nul
 
-if ERRORLEVEL 1 (
-    ECHO.The device %IPAdresse% is NOT available!
+if ERRORLEVEL EQU 0 (
+    ECHO.The device %IPAdresse% is available!
     ECHO.
 ) ELSE (
-    ECHO.The device %IPAdresse% is available!
+    ECHO.The device %IPAdresse% is NOT available!
     ECHO.
 )
 
