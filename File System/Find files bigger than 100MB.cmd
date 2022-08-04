ac7d025207efa66155191f4eb0ec867e74851949
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2022-08-04 16:57:03 +0200

    Update Find files bigger than 100MB.cmd

--- File System/Find files bigger than 100MB.cmd
+++ File System/Find files bigger than 100MB.cmd
@@ -1,2 +1,2 @@
-Find all files which are bigger than 100MB starting at %SystemDrive%
+ECHO Find all files which are bigger than 100MB starting at %SystemDrive%
 forfiles /P %SystemDrive% /S /M * /C "cmd /c if @fsize GEQ 104857600 echo @path"
