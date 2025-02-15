96d808e1c0f92320fcd63f60ef5d708e1d58d24d
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-02-15 14:25:47 +0100

    Update Get MSI product code for an application.cmd

--- Operating System/Windows Installer/Get MSI product code for an application.cmd
+++ Operating System/Windows Installer/Get MSI product code for an application.cmd
@@ -3,4 +3,7 @@ TITLE %~n0
 CLS
 
 REM Get MSI product code for a specific application
-WMIC product WHERE "Name LIKE '%Kaspersky Endpoint Security%'" GET Name, IdentifyingNumber
+WMIC product WHERE "Name LIKE '%TeamViewer%'" GET Name, IdentifyingNumber
+
+REM OR
+WMIC product GET Name,Version,IdentifyingNumber | FIND "TeamViewer"
