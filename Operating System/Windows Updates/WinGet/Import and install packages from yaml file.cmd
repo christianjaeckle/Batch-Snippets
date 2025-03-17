9dc9b261152564641d3bd3246970aeebc1b539bd
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-17 21:29:05 +0100

    Update Import and install packages from yaml file.cmd

--- Operating System/Windows Updates/WinGet/Import and install packages from yaml file.cmd
+++ Operating System/Windows Updates/WinGet/Import and install packages from yaml file.cmd
@@ -1,2 +1,3 @@
 REM Import packages from yaml (DSC) file and install them
+REM Check file "Import and install packages from yaml file.yaml"
 WinGet configure -f "<%UserProfile%\Desktop\Winget_Packages.yaml"
