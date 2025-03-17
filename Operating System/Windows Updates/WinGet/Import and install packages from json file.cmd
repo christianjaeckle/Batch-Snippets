351ffdc671902afc1f20b04989d2a0f6dd64f513
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-17 21:30:46 +0100

    Update Import and install packages from json file.cmd

--- Operating System/Windows Updates/WinGet/Import and install packages from json file.cmd
+++ Operating System/Windows Updates/WinGet/Import and install packages from json file.cmd
@@ -1,2 +1,5 @@
+@ECHO OFF
+TITLE %~n0
+
 REM Import packages from json file and install them
 WinGet Import "%UserProfile%\Desktop\Winget_Package.json" --accept-source-agreements --accept-package-agreements
