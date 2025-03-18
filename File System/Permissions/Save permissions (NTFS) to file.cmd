cc6fc70c3bf30aac6608ce7c36873ccd8399297d
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-03-18 20:58:32 +0100

    Update Save permissions (NTFS) to file.cmd

--- File System/Permissions/Save permissions (NTFS) to file.cmd
+++ File System/Permissions/Save permissions (NTFS) to file.cmd
@@ -3,4 +3,4 @@ TITLE %~n0
 CLS
 
 REM Save permissions of a folder into a text file
-ICacls "%UserProfile%\Desktop" /save ""%UserProfile%\Desktop\Desktop_Folder_Permissions.txt" /t /c
+ICacls "%UserProfile%\Desktop" /save "%UserProfile%\Desktop\Desktop_Folder_Permissions.txt" /t /c
