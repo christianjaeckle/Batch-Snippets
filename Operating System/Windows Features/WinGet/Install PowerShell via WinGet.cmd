873f7a9bbc812590f39123e714cdee72da148ff0
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2025-04-15 12:02:37 +0200

    Update Install PowerShell via WinGet.cmd

--- Operating System/Windows Features/WinGet/Install PowerShell via WinGet.cmd
+++ Operating System/Windows Features/WinGet/Install PowerShell via WinGet.cmd
@@ -2,4 +2,8 @@
 TITLE %~n0
 CLS
 
-WinGet install --id Microsoft.Powershell --source winget
+REM Install Powershell 7 via WinGet
+Winget install --id Microsoft.PowerShell --architecture x64 --scope machine --silent
+
+REM Install Powershell 7 via WinGet with custom configuration
+Winget install --id Microsoft.PowerShell --architecture x64 --scope machine --silent --custom "Use_MU=1 Enable_MU=1 ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=0 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=0 ENABLE_PSREMOTING=1 ADD_PATH=1 DISABLE_TELEMETRY=1"
