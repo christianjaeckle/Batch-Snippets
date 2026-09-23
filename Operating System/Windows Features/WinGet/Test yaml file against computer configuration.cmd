3ff8e39a12340defdc4054106065863b2d31f8fa
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2026-09-23 21:36:31 +0200

    Update Test yaml file against computer configuration.cmd

--- Operating System/Windows Features/WinGet/Test yaml file against computer configuration.cmd
+++ Operating System/Windows Features/WinGet/Test yaml file against computer configuration.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 TITLE %~n0
 
-REM Tests yaml file via WinGet against current system setup
-WinGet configure test -f "<%UserProfile%\Desktop\Winget_Packages.yaml"
+REM Tests yaml (Powershell DSC 3.0) file via WinGet against current system setup.
+WinGet configure test -f "%UserProfile%\Downloads\Winget_Packages.yaml"
