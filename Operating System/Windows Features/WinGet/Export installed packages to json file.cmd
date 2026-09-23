c6db4ee22b15051100333c81ad6fe4ea8214ca69
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2026-09-23 21:24:30 +0200

    Update Export installed packages to json file.cmd

--- Operating System/Windows Features/WinGet/Export installed packages to json file.cmd
+++ Operating System/Windows Features/WinGet/Export installed packages to json file.cmd
@@ -2,4 +2,7 @@
 TITLE %~n0
 
 REM Export current / installed packages to json file
-WinGet Export "%UserProfile%\Desktop\Winget_Package.json"
+WinGet export --output "%UserProfile%\Desktop\Winget_Package.json"
+
+REM Export current / installed packages including versions to json file
+WinGet export --output "%UserProfile%\Desktop\Winget_Package.json" --include-versions
