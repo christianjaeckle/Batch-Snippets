33c77a1088cc74cb9b67888cb07651d9515f5bab
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2026-09-23 22:35:01 +0200

    Update Validate config file.cmd

--- Operating System/Windows Features/WinGet/DSC/Validate config file.cmd
+++ Operating System/Windows Features/WinGet/DSC/Validate config file.cmd
@@ -2,4 +2,4 @@
 TITLE %~n0
 
 REM Validates yaml (Powershell DSC 3.0) file via WinGet.
-WinGet configure validate -f "%UserProfile%\Downloads\Winget_Packages.yaml"
+WinGet configure validate --file "%UserProfile%\Downloads\Winget_Packages.yaml"
