d80b08f1a8bf6a19240d9c3deedd0dd7b02b1af8
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2026-09-23 21:35:57 +0200

    Update Show configuration via yaml file.cmd

--- Operating System/Windows Features/WinGet/DSC/Show configuration via yaml file.cmd
+++ Operating System/Windows Features/WinGet/DSC/Show configuration via yaml file.cmd
@@ -2,4 +2,4 @@
 TITLE %~n0
 
 REM Show configuration via yaml (Powershell DSC 3.0) file
-WinGet configure show -f "<%UserProfile%\Desktop\Winget_Packages.yaml"
+WinGet configure show -f "%UserProfile%\Downloads\Winget_Packages.yaml"
