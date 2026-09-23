559ad080b9f37bce582ecf865194242bff01a742
Christian Jäckle <24988642+christianjaeckle@users.noreply.github.com>
2026-09-23 21:39:25 +0200

    Update Import and install packages from yaml file.cmd

--- Operating System/Windows Features/WinGet/DSC/Import and install packages from yaml file.cmd
+++ Operating System/Windows Features/WinGet/DSC/Import and install packages from yaml file.cmd
@@ -3,4 +3,4 @@ TITLE %~n0
 
 REM Imports packages from a yaml (Powershell DSC 3.0) file and installs those.
 REM Check file "Import and install packages from yaml file.yaml"
-WinGet configure --file "<%UserProfile%\Desktop\Winget_Packages.yaml"
+WinGet configure --file "%UserProfile%\Downloads\Winget_Packages.yaml"
